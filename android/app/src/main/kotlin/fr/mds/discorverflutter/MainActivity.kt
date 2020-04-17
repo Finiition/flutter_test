b84b120858062b75a1c52c26107d7968f50b6c0b
Le Calvez <kevin.lecalvez3@gmail.com>
2020-04-17 10:41:03 +0200

    Add native message

--- android/app/src/main/kotlin/fr/mds/discorverflutter/MainActivity.kt
+++ android/app/src/main/kotlin/fr/mds/discorverflutter/MainActivity.kt
@@ -3,10 +3,21 @@ package fr.mds.discorverflutter
 import androidx.annotation.NonNull;
 import io.flutter.embedding.android.FlutterActivity
 import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugin.common.MethodChannel
 import io.flutter.plugins.GeneratedPluginRegistrant
 
 class MainActivity: FlutterActivity() {
+    private val CHANNEL = "fr.mds.discorverflutter_android/platform_channel";
+
     override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
         GeneratedPluginRegistrant.registerWith(flutterEngine);
+
+        MethodChannel(flutterEngine.dartExecutor.binaryMessenger, CHANNEL).setMethodCallHandler { call, result ->
+
+            //HERE THE CALL HANDLER
+            if(call.method == "helloMessage") {
+                result.success("Hello message from native !")
+            }
+        }
     }
 }
---
+++ lib/platform/platform_channel_helper.dart
@@ -0,0 +1,3 @@
+class plateform_channel_helper {
+  static Future getNativeHello() async {}
+}
